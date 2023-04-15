44423a5a11d44e9ca219d7a89065c789d05b557d
Umar Igan <38042220+UmarIgan@users.noreply.github.com>
2023-04-16 01:05:33 +0300

    Update and rename main.py to Diffusion.py

--- Diffusion.py
+++ Diffusion.py
@@ -1,15 +1,23 @@
 import streamlit as st
 from PIL import Image
 from io import BytesIO
+import torch
 from diffusers import DiffusionPipeline
 from transformers.utils import FLAX_WEIGHTS_NAME
 FLAX_WEIGHTS_NAME = 'flax_model.msgpack'
 
-st.set_page_config(page_title="Text to Image App")
+st.set_page_config(
+    page_title="Diffisun App",
+layout="wide"
+)
 
 # Download the model once and save it to a local directory
+device = "cuda" if torch.cuda.is_available() else "cpu"
 model_id = "CompVis/ldm-text2im-large-256"
-ldm = DiffusionPipeline.from_pretrained(model_id, cache_dir="./models")
+cache_dir = "./models_generator"
+#ldm = DiffusionPipeline.from_pretrained(model_id, cache_dir="models_generator")
+ldm = DiffusionPipeline.from_pretrained(model_id, cache_dir=cache_dir).to(device)
+
 
 st.title("Image Generation App")
 
